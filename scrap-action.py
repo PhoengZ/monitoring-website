bf2f0dcc08f9d7fae9a7650f81f18da9d281b43d
Phaolap Kulteera <pholapcondo11@gmail.com>
2026-03-16 10:38:36 +0700

    add compare function

--- scrap-action.py
+++ scrap-action.py
@@ -1,7 +1,7 @@
 import time
 import requests
 from playwright.sync_api import sync_playwright
-
+import difflib
 
 url = "https://sites.google.com/view/ssukree/courses/2110571-neural-network-22025?authuser=0"
 field = "body"
@@ -22,8 +22,22 @@ def get_content(url, select_field):
             browser.close()
 
 def compare(current, past):
-    return None
+    current_split = current.splitlines()
+    past_split = past.splitlines()
+    # difflib is better than checking index per index because difflib use LCS idea to approach this problem 
+    # for example old content be [a,b,c,d] current content be [X,a,b,c,d] 
+    # if using manual checking it will alert all line are new because old line != current content for each line
+    d = difflib.Differ()
+    # compare(a,b) a is old text and current text b will compare to a 
+    diff = d.compare(past_split, current_split)
 
+    added_content = []
+
+    for line in diff:
+        if line.startswith("+ "):
+            added_content.append(line)
+    
+    return added_content
 
 def loop():
     content = get_content(url, field)
@@ -34,17 +48,16 @@ def loop():
     except FileNotFoundError as fe:
         print(f"File note found: {fe}")
         last_content = ""
-    # if content != last_content:
-    #     print("Something Update")
+    if content != last_content:
+        print("Something Update")
 
-    #     with open(file, "w") as f:
-    #         f.write(content)
-    #         f.close()
-    split_current = content.splitlines()
-    split_past = last_content.splitlines()
-    print(split_current)
-    print('----------------------------------------')
-    print(split_past)
+        with open(file, "w") as f:
+            f.write(content)
+            f.close()
+
+        add_content = compare(current=content, past=last_content)
+        for change in add_content:
+            print(f'มีการเปลี่ยนแปลงดังนี้: {change}')
 
 if __name__ == '__main__':
     loop()
